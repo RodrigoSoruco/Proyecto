9720552ae439aeb080bdf7b6fba285a8f312c95c
RodrigoSoruco <rodrigo_soruco@outlook.com>
2020-10-07 02:12:24 -0300

    Modification 07/10/20

--- guessinggame.sh
+++ guessinggame.sh
@@ -6,27 +6,32 @@
 function guess {
 	while true
 	do
-		if [[ $number -eq $num_files ]]
+		if [[ $number =~ $numbers ]]
 		then
-			echo "Correct."
-			break
-		elif [[ $number -lt $num_files ]]
-		then
-			echo "To low, please try again."
+			if [[ $number -eq $num_files ]]
+			then
+				echo "Correct."
+				break
+			elif [[ $number -lt $num_files ]]
+			then
+				echo "To low, please try again."
+			else
+				echo "To high, please try again."
+			fi
 		else
-			echo "To high, please try again."
+			echo "The entered value is not valid, enter a number."
 		fi
-
-		read number
-
+			read number
 	done
 }
 
 echo "Please type number of files in this directory?"
 read number
 
-num_files=3
+num_files=$(ls | wc -l)
+numbers="^[0-9]+$"
 
 guess
 
-echo "End of program."
+echo "End of program"
+
